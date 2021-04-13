c5c2d12b33ad52effb5479476ab3441fa6c317e2
uraharcik <castirl15@gmail.com>
2021-04-13 18:36:20 +0300

    Adding html

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Movie
+
+
+class MovieView(ListView):
+    """Movie list"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'movie/base.html'
+
+
+class MovieDetailView(DetailView):
+    """ Movie description"""
+    model = Movie
+    slug_field = "url"
+
+
